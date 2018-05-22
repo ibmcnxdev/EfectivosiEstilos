8b1390677872d488169bedb63fbc9cf99533c9da
Juan Leon <jleon@cl.ibm.com>
2018-05-22 10:14:05 -0400

    new layout

--- pinnedcomms/pinnedcomms.js
+++ pinnedcomms/pinnedcomms.js
@@ -9,7 +9,7 @@
 //
 // @author       jleon@cl.ibm.com
 //
-// @include      *://apps.collabservintegration.com/*  << Global
+// @include      *://apps.collabservintegration.com/communities/*  << for an specific community
 //
 // @exclude
 //
@@ -19,17 +19,35 @@
 
 var titulos = [
     "Nueva Forma de Trabajar",
-    "IBM Cloud"
+    "IBM Cloud",
+    "Mi compania HM",
+    "Semana de la Salud",
+    "Egresados Universitarios",
+    "Recursos Humanos IBM",
+    "Yo sé quien sabe lo que usted no sabe",
+    "Fábrica de Cajas"
 ];
 
 var commids = [
     "7d04739c-be2e-476f-9423-fc559de80cd9",
-    "7414b19c-98d2-4f2c-b145-b86b92471e68"
+    "7414b19c-98d2-4f2c-b145-b86b92471e68",
+    "34749635-8269-4ac8-92a9-150e6de762bb",
+    "b0a9f5ab-ae93-4881-a3b0-6bc570c3ccaf",
+    "1e50397b-8f8b-41db-b69f-b838b8916fe9",
+    "9aaf9f0f-06b6-4c1d-b93c-615c4ee646e8",
+    "14deabef-1e21-4fdc-8950-230b2b461f10",
+    "2656dd49-5f9a-449c-859d-bda5781c625e"
 ];
 
 var commdescs = [
-    "Esto es una comunidad destacada",
-    "Esto es otra comunidad destacada"
+    "Una nueva forma de trabajar para todos",
+    "Todos sobre la nube",
+    "Comunicacion interna de historiales",
+    "Todo sobre la semana de la salud",
+    "Comunidad de egresados universitarios",
+    "Comunidad oficial de RRHH",
+    "Aprenda como buscar y sepa a quien preguntar",
+    "El emocionante proceso de fabricación de cajas"
 ];
 
 var checkExist = setInterval(function() {
@@ -38,16 +56,31 @@ var checkExist = setInterval(function() {
         var node = document.createElement("div");
         //node.innerHTML = html;
 
-        var i;
+        var i, cont = 0;
         var html = `
         <h2 class="lotusHeading" id="lotusHeadingH2ID" style="padding: 5px 0px 22px;">
             <span class="lotusText">Comunidades destacadas</span>
         </h2>
+        <table  class="lotusTable" role="presentation" cellspacing="0" cellpadding="0" border="0"> 
+        <tbody>
         `;
-        for (i = 0; i < titulos.length; i++) { 
-            var html = html + genHTML(titulos[i], commids[i], commdescs[i]);
+        for (i = 0; i < titulos.length; i++) {
+            if (cont == 0 ) {
+                var html = html + `<tr class="placeRow lotusFirst" role="article" aria-labelledby="lconn_communities_catalog_widgets_PlaceDisplayWidget_1-title">`;
+                var html = html + genHTML(titulos[i], commids[i], commdescs[i]);
+                cont++;
+            } else if (cont == 2 ) {
+                var html = html + genHTML(titulos[i], commids[i], commdescs[i]);
+                var html = html + `</tr>`;
+                cont = 0;
+            } else {
+                var html = html + genHTML(titulos[i], commids[i], commdescs[i]);
+                cont++;
+            }
         }
         var html = html + `
+        </tbody>
+        </table>
         <h2 class="lotusHeading" id="lotusHeadingH2ID" style="padding: 5px 0px 5px;">
             <span class="lotusText">Comunidades</span>
         </h2>
@@ -61,9 +94,6 @@ var checkExist = setInterval(function() {
 
 function genHTML(titulo, commid, descripcion) {
     var html = `
-    <table  class="lotusTable" role="presentation" cellspacing="0" cellpadding="0" border="0"> 
-        <tbody>
-            <tr class="placeRow lotusFirst" role="article" aria-labelledby="lconn_communities_catalog_widgets_PlaceDisplayWidget_1-title">
                 <td class="lotusFirstCell" dojoattachpoint="communityIcon" width="35px">
                     <img style="border-radius: 50%;" src="/communities/service/html/image?communityUuid=`+commid+`" alt="" role="presentation" width="64" height="64">
                 </td>
@@ -84,9 +114,6 @@ function genHTML(titulo, commid, descripcion) {
                         <p dojoattachpoint="placeDescNode" class="bidiAware">Comunidad destacada</p>
                     </div>
                 </td>
-            </tr>
-        </tbody>
-    </table>
     `;
     return(html);
 }
